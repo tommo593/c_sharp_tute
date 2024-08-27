0a2813123d452168a1514464dceb24c2c15879b0
Tommo <113673885+tommo593@users.noreply.github.com>
2024-08-27 22:07:00 +0100

    Add files via upload

---
+++ c_sharp_tute/c_sharp_tute/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+            }
+    }
+}
